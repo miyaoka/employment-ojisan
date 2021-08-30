ea612c3e71ecd9e17b3702afd7ad6380ef758d87
Masaya Kazama <miyan@t-p.jp>
2021-08-31 02:34:37 +0900

    add twitter share button

--- src/components/CountDown.tsx
+++ src/components/CountDown.tsx
@@ -8,6 +8,10 @@ export type TimeDiff = {
 
 export default function CountDown(diff: TimeDiff) {
   const { sec, min, hour, date, isBefore } = diff;
+  const preText = `就職${isBefore ? "するまで、あと" : "してから"}`;
+  const time = `${date}日${hour}時間${min}分${sec}秒`;
+  const postText = `${isBefore ? "です" : "経ちました"}。`;
+  const tweetText = `統合開発環境 (@sadnessOjisan) さんが${preText}${time}${postText}`;
   return (
     <div>
       <img
@@ -28,9 +32,26 @@ export default function CountDown(diff: TimeDiff) {
         @sadnessOjisan
       </a>
       ) さんが
-      <div>就職{isBefore ? `するまで、あと` : `してから`}</div>
-      <div className="text-[96px]">{`${date}日${hour}時間${min}分${sec}秒`}</div>
-      <div>{isBefore ? `です` : `経ちました`}。</div>
+      <div>{preText}</div>
+      <div className="text-[96px]">{time}</div>
+      <div>{postText}</div>
+      <div className="flex justify-center">
+        <a
+          href={`https://twitter.com/intent/tweet?text="${tweetText}"%0ahttps://employment-ojisan.vercel.app/`}
+          target="_blank"
+          rel="noopener noreferrer"
+          className="flex items-center flex-col"
+          title="Share on Twitter"
+        >
+          <div
+            className="h-10 w-10 bg-gray-700"
+            style={{
+              WebkitMask: `url(/images/twitter.svg) no-repeat center / contain`,
+            }}
+          />
+          シェアする
+        </a>
+      </div>
     </div>
   );
 }
