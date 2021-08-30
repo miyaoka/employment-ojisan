0cb8518970b89c18c38529f44cc7071d440c01d9
Masaya Kazama <miyan@t-p.jp>
2021-08-31 00:15:15 +0900

    add tailwind

---
+++ src/pages/index.tsx
@@ -0,0 +1,20 @@
+import type { NextPage } from "next";
+import Head from "next/head";
+
+const Home: NextPage = () => {
+  return (
+    <div className="">
+      <Head>
+        <title>employment-ojisan count down</title>
+        <meta name="description" content="employment-ojisan count down" />
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+
+      <main className="">main</main>
+
+      <footer className="">footer</footer>
+    </div>
+  );
+};
+
+export default Home;
