93f8f874e597cca52815ce3d72c32add1908ad92
Masaya Kazama <miyan@t-p.jp>
2021-08-31 10:22:20 +0900

    add just employed screen

--- src/components/CountDown.tsx
+++ src/components/CountDown.tsx
@@ -1,59 +1,98 @@
-export type TimeDiff = {
+type TimeDiff = {
   sec: number;
   min: number;
   hour: number;
   date: number;
-  isBefore: boolean;
 };
 
-export default function CountDown(diff: TimeDiff) {
-  const { sec, min, hour, date, isBefore } = diff;
+const getTimeDiff = (diff: number): TimeDiff => {
+  const time = Math.abs(diff);
+  const sec = time % 60;
+  const min = Math.floor(time / 60) % 60;
+  const hour = Math.floor(time / 3600) % 24;
+  const date = Math.floor(time / 86400);
+  return { date, hour, min, sec };
+};
+
+const name = (
+  <div>
+    統合開発環境 (
+    <a
+      href="https://twitter.com/sadnessOjisan"
+      target="_blank"
+      rel="noopener noreferrer"
+    >
+      @sadnessOjisan
+    </a>
+    ) さんが
+  </div>
+);
+const shareOnTwitter = (tweetText: string) => {
+  const name = "統合開発環境 (@sadnessOjisan) さんが";
+  return (
+    <div className="flex justify-center">
+      <a
+        href={`https://twitter.com/intent/tweet?hashtags=syusyoku_20210901&text=${name}${tweetText}"&url=https://employment-ojisan.vercel.app/`}
+        target="_blank"
+        rel="noopener noreferrer"
+        className="flex items-center flex-col"
+        title="Share on Twitter"
+      >
+        <div
+          className="h-10 w-10 bg-gray-700"
+          style={{
+            WebkitMask: `url(/images/twitter.svg) no-repeat center / contain`,
+          }}
+        />
+        シェアする
+      </a>
+    </div>
+  );
+};
+
+export default function CountDown(timeFromTarget: number) {
+  const { sec, min, hour, date } = getTimeDiff(timeFromTarget);
+
+  if (timeFromTarget >= 0 && timeFromTarget < 10) {
+    const syusyokuNow = [...new Array(Math.floor((timeFromTarget + 1) ** 1.7))]
+      .map((_) => "就職！")
+      .join("");
+    return (
+      <div>
+        {name}
+        <img
+          src={`/images/message_syusyoku_omedetou.png`}
+          className="fixed left-0 top-0 z-[-1] object-contain opacity-50 w-screen h-screen"
+        />
+        <div className="text-[32px] sm:text-[64px] md:text-[72px] lg:text-[96px] font-bold leading-none text-red-500">
+          {syusyokuNow}
+        </div>
+        <div>しました。</div>
+        {shareOnTwitter(`${syusyokuNow}しました`)}
+      </div>
+    );
+  }
+  const isBefore = timeFromTarget < 0;
   const preText = `就職${isBefore ? "するまで、あと" : "してから"}`;
   const time = `${date}日${hour}時間${min}分${sec}秒`;
   const postText = `${isBefore ? "です" : "経ちました"}。`;
-  const tweetText = `統合開発環境 (@sadnessOjisan) さんが${preText}${time}${postText}`;
+  const tweetText = `${preText}${time}${postText}`;
+  const imgSrc = `/images/${
+    isBefore ? "syusyoku_nayamu_neet_man.png" : "message_syusyoku_omedetou.png"
+  }`;
   return (
     <div>
       <img
-        src={`/images/${
-          isBefore
-            ? "syusyoku_nayamu_neet_man.png"
-            : "message_syusyoku_omedetou.png"
-        }`}
-        alt=""
+        src={imgSrc}
         className="fixed left-0 top-0 z-[-1] object-contain opacity-25 w-screen h-screen"
       />
-      統合開発環境 (
-      <a
-        href="https://twitter.com/sadnessOjisan"
-        target="_blank"
-        rel="noopener noreferrer"
-      >
-        @sadnessOjisan
-      </a>
-      ) さんが
+      {name}
       <div>{preText}</div>
       <div className="text-[32px] sm:text-[64px] md:text-[72px] lg:text-[96px] font-bold">
         {time}
       </div>
       <div>{postText}</div>
-      <div className="flex justify-center">
-        <a
-          href={`https://twitter.com/intent/tweet?hashtags=syusyoku_20210901&text="${tweetText}"%0ahttps://employment-ojisan.vercel.app/`}
-          target="_blank"
-          rel="noopener noreferrer"
-          className="flex items-center flex-col"
-          title="Share on Twitter"
-        >
-          <div
-            className="h-10 w-10 bg-gray-700"
-            style={{
-              WebkitMask: `url(/images/twitter.svg) no-repeat center / contain`,
-            }}
-          />
-          シェアする
-        </a>
-      </div>
+      {shareOnTwitter(tweetText)}
     </div>
   );
 }
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,32 +1,22 @@
 import type { NextPage } from "next";
 import Head from "next/head";
-import { useEffect, useState } from "react";
-import CountDown, { TimeDiff } from "../components/CountDown";
+import { useState } from "react";
+import CountDown from "../components/CountDown";
 
 const employmentTime = new Date("2021-09-01T00:00:00+09:00").getTime();
-const getTimeForEmployment = () =>
-  Math.floor((employmentTime - Date.now()) / 1000);
-
-const getTimeDiff = (diff: number): TimeDiff => {
-  const time = Math.abs(diff);
-  const sec = time % 60;
-  const min = Math.floor(time / 60) % 60;
-  const hour = Math.floor(time / 3600) % 24;
-  const date = Math.floor(time / 86400);
-  return { date, hour, min, sec, isBefore: diff > 0 };
-};
+const getTimeFromEmployment = () =>
+  Math.floor((Date.now() - employmentTime) / 1000);
 
 const ogImage = "https://i.imgur.com/9XZ9quS.png";
 const title = "@sadnessOjisan 就職タイマー";
 const desc = "社会性へのカウントダウン";
 
 const Home: NextPage = () => {
-  const [time, setTime] = useState(getTimeForEmployment());
-  useEffect(() => {
-    setInterval(() => {
-      setTime(getTimeForEmployment());
-    }, 1000);
-  }, []);
+  const [timeFromEmployment, setTime] = useState(getTimeFromEmployment());
+
+  setInterval(() => {
+    setTime(getTimeFromEmployment());
+  }, 1000);
 
   return (
     <div className="p-4 sm:p-10">
@@ -43,7 +33,7 @@ const Home: NextPage = () => {
           content="summary_large_image"
         />
       </Head>
-      <main>{CountDown(getTimeDiff(time))}</main>
+      <main>{CountDown(timeFromEmployment)}</main>
     </div>
   );
 };
