37f6e6841b3c222172bd98519c17f745780db003
Masaya Kazama <miyan@t-p.jp>
2021-08-31 23:19:09 +0900

    add celebrating sound effect

--- src/components/BgmPlayer.tsx
+++ src/components/BgmPlayer.tsx
@@ -6,42 +6,59 @@ type YouTubeIFramePlayer = {
   playVideo: () => void;
   pauseVideo: () => void;
   seekTo: (seconds: number, allowSeekAhead: boolean) => void;
-}
+};
 
-function assertYouTubeIFramePlayer (player?: unknown): player is YouTubeIFramePlayer {
+function assertYouTubeIFramePlayer(
+  player?: unknown
+): player is YouTubeIFramePlayer {
   return !!player;
 }
 
 type Props = {
-  status: Status
+  status: Status;
 };
 const BgmPlayer: React.FC<Props> = ({ status }) => {
   const [player, setPlayer] = useState<any>(null);
+  const [player2, setPlayer2] = useState<any>(null);
 
   useEffect(() => {
-    const playable = assertYouTubeIFramePlayer(player)
+    const playable =
+      assertYouTubeIFramePlayer(player) && assertYouTubeIFramePlayer(player2);
     if (!playable) {
       return;
     }
     if (status === "countingDown") {
       player.playVideo();
     }
+    if (status === "celebrating") {
+      player2.playVideo();
+    }
     if (status === "working") {
       player.pauseVideo();
-      player.seekTo(0, false);
+      player2.pauseVideo();
     }
-  }, [status, player]);
+  }, [status, player, player2]);
 
-  const onReady = (evt: Event) => {
+  const onReadyCountDown = (evt: Event) => {
     setPlayer(evt.target);
+  };
+  const onReadyCelebrate = (evt: Event) => {
+    setPlayer2(evt.target);
   };
 
   return (
-    <YouTube
-      videoId="PS2qPYbzEb4"
-      onReady={onReady}
-      className="opacity-0 pointer-events-none fixed bottom-0"
-    />
+    <>
+      <YouTube
+        videoId="PS2qPYbzEb4"
+        onReady={onReadyCountDown}
+        className="opacity-0 pointer-events-none fixed bottom-0"
+      />
+      <YouTube
+        videoId="1ARb7r0yY9k"
+        onReady={onReadyCelebrate}
+        className="opacity-0 pointer-events-none fixed bottom-0"
+      />
+    </>
   );
 };
 
