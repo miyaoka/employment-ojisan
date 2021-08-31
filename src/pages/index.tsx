3ee1e3a4446edf7e76d739b1f6cffb2f0cb620b4
Masaya Kazama <miyan@t-p.jp>
2021-08-31 11:00:33 +0900

    add repository link

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -49,6 +49,17 @@ const Home: NextPage = () => {
       >
         {process.browser && CountDown(timeFromEmployment)}
       </main>
+      <footer>
+        <a
+          href="https://github.com/miyaoka/employment-ojisan"
+          target="_blank"
+          rel="noopener noreferrer"
+          title="repository"
+          className="fixed right-4 bottom-4 w-[40px] h-[40px]"
+        >
+          <img src="/images/GitHub-Mark-64px.png" />
+        </a>
+      </footer>
     </div>
   );
 };
