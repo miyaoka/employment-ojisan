16dac7aa2d8f3e8d3124078b15c543316581e286
Masaya Kazama <miyan@t-p.jp>
2021-08-31 16:40:31 +0900

    play countdown music

---
+++ src/components/BgmPlayer.tsx
@@ -0,0 +1,30 @@
+import React, { useEffect, useState } from "react";
+import YouTube from "react-youtube";
+
+type Props = {
+  msFromTarget: number;
+};
+const BgmPlayer: React.FC<Props> = ({ msFromTarget }) => {
+  // 初期値が-10秒以下なら未プレイにする
+  const [isPlaying, setPlaying] = useState(-msFromTarget < 10000);
+  const [player, setPlayer] = useState<any>(null);
+
+  useEffect(() => {
+    if (isPlaying) return;
+    // 残りが10秒台以下になったら再生
+    if (Math.floor(msFromTarget / -1000) <= 10) {
+      setPlaying(true);
+      player?.playVideo();
+    }
+  }, [msFromTarget]);
+
+  const onReady = (evt: Event) => {
+    setPlayer(evt.target);
+  };
+
+  return (
+    <YouTube videoId="PS2qPYbzEb4" onReady={onReady} className="sr-only" />
+  );
+};
+
+export default BgmPlayer;
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,6 +1,7 @@
 import type { NextPage } from "next";
 import Head from "next/head";
 import { useState } from "react";
+import BgmPlayer from "../components/BgmPlayer";
 import CountDown from "../components/CountDown";
 import { useAnimationFrame } from "../hooks/animationFrame";
 
@@ -38,6 +39,7 @@ const Home: NextPage = () => {
         className="text-lg"
       >
         {process.browser && <CountDown msFromTarget={timeFromEmployment} />}
+        <BgmPlayer msFromTarget={timeFromEmployment} />
       </main>
       <footer>
         <a
